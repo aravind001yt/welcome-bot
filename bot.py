17ef57ae9fde72525fcbb02500f3558657f6f023
aravind001yt <92426188+aravind001yt@users.noreply.github.com>
2021-10-30 17:11:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''Hi I am Welcome Bot I am Welcome Ur Members When Join
+ updater.message.reply_text('''Hi I am Welcome Bot I am Welcome Ur Members When Join. Add Me In Your Group To Make Your Group To Super Group
  
  Bot Owner @ELECTRO_444
 
@@ -17,7 +17,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to EO MANGER BOT Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Welcome to {chatname} Thank you for Joining  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
